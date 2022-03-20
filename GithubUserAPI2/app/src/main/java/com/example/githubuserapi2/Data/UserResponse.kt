a2d02816c7eea82c6c8f22ec202485aa7c73da1f
aril941 <muhammadaril5525@gmail.com>
2022-03-20 09:14:33 +0700

    del

--- GithubUserAPI2/app/src/main/java/com/example/githubuserapi2/Data/UserResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.githubuserapi2.Data
-
-import com.google.gson.annotations.SerializedName
-
-data class UserResponse(
-
-    @field:SerializedName("items")
-    val items: ArrayList<User>
-)
